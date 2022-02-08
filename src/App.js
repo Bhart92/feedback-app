95c9f91e36648e2b9d8df393d854dfaad866068f
Brandon H <hartwebdev92@gmail.com>
2022-02-08 10:53:31 -0800

    added header component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Header from "./components/Header";
+
 function App() {
     const title = "blog post";
     const body = "blog body";
@@ -23,11 +25,14 @@ function App() {
     );
 
     return (
+        <>
+        <Header text="Header"/>
         <div className="container">
             <h1>{title}</h1>
             <p>{body}</p>
             {showComments && commentBlock}
         </div>
+        </>
     )
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+function Header({ text }) {
+  return (
+      <header>
+        <div className="container">
+            <h2>{ text }</h2>
+        </div>
+      </header>
+  )
+};
+
+Header.defaultProps = {
+    text: "feedback UI"
+}
+
+export default Header;
