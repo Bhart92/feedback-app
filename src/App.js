19908a566565f9c6d397aa519d114c4eef840ce7
Brandon H <hartwebdev92@gmail.com>
2022-02-08 09:33:10 -0800

    set up app component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 function App() {
+    const title = "blog post";
+    const body = "blog body";
+    const comments = [
+        {id: 1, text: "some 1"},
+        {id: 2, text: "some 2"},
+        {id: 3, text: "some 3"},
+
+    ];
     return (
-        <h1>Hello from App Component</h1>
+        <div className="container">
+            <h1>{title}</h1>
+            <p>{body}</p>
+            <div className="comments">
+                <h3>Comments: ({comments.length})</h3>
+                <ul>
+                    {comments.map((comment, index) => (
+                        <li key={index}>{comment.text}</li>
+                    ))}
+                </ul>
+            </div>
+
+        </div>
     )
 }
 
