fbf8aadadb73ced14ffb14d89ee800ed2b0705d0
Brandon H <hartwebdev92@gmail.com>
2022-02-08 09:52:58 -0800

    variables containing jsx

--- src/App.js
+++ src/App.js
@@ -5,21 +5,28 @@ function App() {
         {id: 1, text: "some 1"},
         {id: 2, text: "some 2"},
         {id: 3, text: "some 3"},
-
     ];
+
+    const loading = false;
+    const showComments = true;
+    if(loading) return <h1>Loading...</h1>;
+
+    const commentBlock = (
+        <div className="comments">
+            <h3>Comments: ({comments.length})</h3>
+            <ul>
+                {comments.map((comment, index) => (
+                    <li key={index}>{comment.text}</li>
+                ))}
+            </ul>
+        </div>
+    );
+
     return (
         <div className="container">
             <h1>{title}</h1>
             <p>{body}</p>
-            <div className="comments">
-                <h3>Comments: ({comments.length})</h3>
-                <ul>
-                    {comments.map((comment, index) => (
-                        <li key={index}>{comment.text}</li>
-                    ))}
-                </ul>
-            </div>
-
+            {showComments && commentBlock}
         </div>
     )
 }
