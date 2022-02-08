12c6541bbf5469336eb1bda4d2dfd7af8b543949
Brandon H <hartwebdev92@gmail.com>
2022-02-08 10:25:19 -0800

    dev branch go byebye

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
 function App() {
-    const title = "blog post";
-    const body = "blog body";
-    const comments = [
-        {id: 1, text: "some 1"},
-        {id: 2, text: "some 2"},
-        {id: 3, text: "some 3"},
-    ];
-
-    const loading = false;
-    const showComments = true;
-    if(loading) return <h1>Loading...</h1>;
-
-    const commentBlock = (
-        <div className="comments">
-            <h3>Comments: ({comments.length})</h3>
-            <ul>
-                {comments.map((comment, index) => (
-                    <li key={index}>{comment.text}</li>
-                ))}
-            </ul>
-        </div>
-    );
 
     return (
         <div className="container">
-            <h1>{title}</h1>
-            <p>{body}</p>
-            {showComments && commentBlock}
+            <h1>App</h1>
         </div>
     )
 }
