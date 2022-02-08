761909735cc05495d3644f8e16ac287853fa01ce
Brandon H <hartwebdev92@gmail.com>
2022-02-08 11:06:22 -0800

    merge main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import Header from "./components/Header";
 
 function App() {
+    const title = "blog post";
+    const body = "blog body";
+    const comments = [
+        {id: 1, text: "some 1"},
+        {id: 2, text: "some 2"},
+        {id: 3, text: "some 3"},
+    ];
+
+    const loading = false;
+    const showComments = true;
+    if(loading) return <h1>Loading...</h1>;
+
+    const commentBlock = (
+        <div className="comments">
+            <h3>Comments: ({comments.length})</h3>
+            <ul>
+                {comments.map((comment, index) => (
+                    <li key={index}>{comment.text}</li>
+                ))}
+            </ul>
+        </div>
+    );
 
     return (
         <>
         <Header text="Header"/>
         <div className="container">
-            <h1>App</h1>
+            <h1>{title}</h1>
+            <p>{body}</p>
+            {showComments && commentBlock}
         </div>
         </>
     )
